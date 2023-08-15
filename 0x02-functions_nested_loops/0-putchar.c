8826def96818e9ea4a533cfc2f61174d8bfe1358
Ahmed Emad ElDeen <ahmmedemmad2@gmail.com>
2023-08-15 09:58:30 +0300

    aa

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char *str = "_putchar\n";
+
+	while (*str)
+	{
+		write(1, str, 1);
+		str++;
+	}
+
+	return (0);
+}
