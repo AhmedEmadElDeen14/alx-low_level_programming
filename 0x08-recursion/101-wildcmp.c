fa65ad2dc8a1ee4515f80a2ba6c65f36d1ff00cc
Ahmed Emad ElDeen <ahmmedemmad2@gmail.com>
2023-08-30 21:03:31 +0300

    100-wildcmp

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,13 +1,19 @@
-int is_palindrome(char *s) {
-  
-  if (strlen(s) <= 1) {
-    return 1;
-  }
-
-  
-  if (s[0] != s[strlen(s) - 1]) {
-    return 0;
-  }
-
-  return is_palindrome(s + 1, s - 1);
+#include "holberton.h"
+/**
+ * wildcmp -  compare 2 string and search if s2 is a valid wildcard string
+ * @s1: string.
+ * @s2: string with wildcard.
+ * Return: 1 if the strings can be considered identical, otherwise return 0.
+ */
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == '\0' && *s2 == '*' && *(s2 + 1) != '\0')
+		return (0);
+	if (*s1 == '\0' && *s2 == '\0')
+		return (1);
+	if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+	if (*s2 == '*')
+		return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
+	return (0);
 }
